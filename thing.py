9d9bb1a483a705238b996f68ed9397e2487187e9
CMSBill <bill@cmsnova.com>
2025-02-04 20:11:43 +0000

    fix

--- thing.py
+++ thing.py
@@ -1,34 +1,34 @@
-import re;fjqweipqew = ""
-for fqwjiofqwe in range(100):
-    fjqweipqew += "1"
-n=2;jfiewqojfqiewofeqwfewq = r"11";jifowqjfiqe = re.sub(jfiewqojfqiewofeqwfewq, "10", fjqweipqew, 0);print(jifowqjfiqe);j=1;n+=1;fejqwiofqwe = jifowqjfiqe;efjqwiopqfwe = ""
-while n <= 99:
-    for fqwjiofqwe in range(100):
-        if fqwjiofqwe == 0 or fqwjiofqwe%n==0:
-            jfiowqejqofqej = (fejqwiofqwe[fqwjiofqwe]);
-            if jfiowqejqofqej == "1":
-                efjqwiopqfwe+="0";
+import re;jefioqwjfioqwejfoqe = ""
+for fewqfqewfjqewifjqweofweqiofjiqowefjoippp in range(100):
+    jefioqwjfioqwejfoqe += "1"
+fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo=2;jfiewqojfqiewofeqwfewq = r"11";jifowqjfiqe = re.sub(jfiewqojfqiewofeqwfewq, "10", jefioqwjfioqwejfoqe, 0);print(jifowqjfiqe);jfqweifjeiwqofjweiqojfeiwqojfewqio=1;fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo+=1;fjeqwoifjiqowejfqwejfoewqjfiqowe = jifowqjfiqe;jfiweoqfjieoqwjfiqowejfiowqe = ""
+while fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo <= 99:
+    for fewqfqewfjqewifjqweofweqiofjiqowefjoippp in range(100):
+        if fewqfqewfjqewifjqweofweqiofjiqowefjoippp == 0 or fewqfqewfjqewifjqweofweqiofjiqowefjoippp%fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo==0:
+            jfioewjfioqwejioweqjfioqwefewqfioqew = (fjeqwoifjiqowejfqwejfoewqjfiqowe[fewqfqewfjqewifjqweofweqiofjiqowefjoippp]);
+            if jfioewjfioqwejioweqjfioqwefewqfioqew == "1":
+                jfiweoqfjieoqwjfiqowejfiowqe+="0";
             else:
-                efjqwiopqfwe+="1";
+                jfiweoqfjieoqwjfiqowejfiowqe+="1";
         else:
-            efjqwiopqfwe+= (fejqwiofqwe[fqwjiofqwe]);
-    print(f"{efjqwiopqfwe}, {n}");fejqwiofqwe=efjqwiopqfwe;n+=1 ;efjqwiopqfwe="";
-jifowqjfiqe = fejqwiofqwe;fjeqwiofqweji=0;fejqwiofqwe="";
-for fqwjiofqwe in range(100):
-    if fqwjiofqwe != 99 or fqwjiofqwe != 0:
-        fejqwiofqwe+=jifowqjfiqe[fqwjiofqwe];
+            jfiweoqfjieoqwjfiqowejfiowqe+= (fjeqwoifjiqowejfqwejfoewqjfiqowe[fewqfqewfjqewifjqweofweqiofjiqowefjoippp]);
+    print(f"{jfiweoqfjieoqwjfiqowejfiowqe}, {fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo}");fjeqwoifjiqowejfqwejfoewqjfiqowe=jfiweoqfjieoqwjfiqowejfiowqe;fdjfiowqejojfwqifjeioqwjfoiqweiofiewqjfiwoqejfioewqjfqweo+=1 ;jfiweoqfjieoqwjfiqowejfiowqe="";
+jifowqjfiqe = fjeqwoifjiqowejfqwejfoewqjfiqowe;fjeqwiofqweji=0;fjeqwoifjiqowejfqwejfoewqjfiqowe="";
+for fewqfqewfjqewifjqweofweqiofjiqowefjoippp in range(100):
+    if fewqfqewfjqewifjqweofweqiofjiqowefjoippp != 99 or fewqfqewfjqewifjqweofweqiofjiqowefjoippp != 0:
+        fjeqwoifjiqowejfqwejfoewqjfiqowe+=jifowqjfiqe[fewqfqewfjqewifjqweofweqiofjiqowefjoippp];
     else:
-        if jifowqjfiqe[fqwjiofqwe] == "1":
-            fejqwiofqwe+="0";
+        if jifowqjfiqe[fewqfqewfjqewifjqweofweqiofjiqowefjoippp] == "1":
+            fjeqwoifjiqowejfqwejfoewqjfiqowe+="0";
         else:
-            fejqwiofqwe+="1";
-jifowqjfiqe=fejqwiofqwe;
+            fjeqwoifjiqowejfqwejfoewqjfiqowe+="1";
+jifowqjfiqe=fjeqwoifjiqowejfqwejfoewqjfiqowe;
 
-for fqwjiofqwe in jifowqjfiqe:
-    if fqwjiofqwe == "1":
+for fewqfqewfjqewifjqweofweqiofjiqowefjoippp in jifowqjfiqe:
+    if fewqfqewfjqewifjqweofweqiofjiqowefjoippp == "1":
         fjeqwiofqweji+=1;
 print(f"There are {fjeqwiofqweji} closed lockers.");fjeqwiofqweji=0;
-for fqwjiofqwe in jifowqjfiqe:
-    if fqwjiofqwe == "0":
+for fewqfqewfjqewifjqweofweqiofjiqowefjoippp in jifowqjfiqe:
+    if fewqfqewfjqewifjqweofweqiofjiqowefjoippp == "0":
         fjeqwiofqweji+=1;
 print(f"There are {fjeqwiofqweji} open lockers.");
